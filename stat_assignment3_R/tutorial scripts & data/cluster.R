59be668d0b9ab947eb7191742c0c5f4293b2f84a
jonatronblah <jonatron@gmail.com>
2019-02-08 16:57:26 -0500

    n/a

---
+++ stat_assignment3_R/tutorial scripts & data/cluster.R
@@ -0,0 +1,54 @@
+#Cluster Analysis
+
+#Read in data
+college.data <- read_csv("C:/Users/jonathan/Desktop/DAPT Docs/spring 2019/Statistics/stat_assignments_spring19_R/stat_assignment3_R/tutorial scripts & data/CollUniv.txt")
+college.data$AccRate <- as.numeric(sub("%","",college.data$AccRate))
+college.data$ExpPerStudent <- as.numeric(as.character(sub("\\$","",college.data$ExpPerStudent)))
+
+#Hierarchical Clustering
+d <- dist(college.data[,3:7],method="euclidean")
+hier.clust <- hclust(d,method="ward.D")
+plot(hier.clust)
+groups <- cutree(hier.clust,k=5)
+rect.hclust(hier.clust,k=5,border="red")
+aggregate(college.data[,3:7],list(groups),FUN=mean)
+
+#Two-way clustering
+mydata <- as.matrix(college.data[,3:7])
+rownames(mydata) <- college.data$School
+datascaled <- scale(mydata)
+heatmap(datascaled)
+
+#K-means clustering
+library(factoextra)
+fviz_nbclust(college.data[,3:7],kmeans,method="gap_stat")
+kmean.clust <- kmeans(college.data[,3:7],5)
+aggregate(mydata,by=list(kmean.clust$cluster),FUN=mean)
+new.data <- data.frame(college.data, kmean.clust$cluster)
+fviz_cluster(kmean.clust,data=college.data[,3:7])
+
+####################################################################
+# Car Data
+
+cars <- read_csv("C:/Users/jonathan/Desktop/DAPT Docs/spring 2019/Statistics/stat_assignments_spring19_R/stat_assignment3_R/tutorial scripts & data/cars_mod.txt")
+
+#Hierarchical Clustering
+d <- dist(cars[,3:16],method="euclidean")
+hier.clust <- hclust(d,method="ward.D")
+plot(hier.clust)
+groups <- cutree(hier.clust,k=5)
+rect.hclust(hier.clust,k=5,border="red")
+aggregate(cars[,3:16],list(groups),FUN=mean)
+
+#Two-way clustering
+mydata <- as.matrix(cars[,3:16])
+rownames(mydata) <- cars$make
+datascaled <- scale(mydata)
+heatmap(datascaled)
+
+#K-means clustering
+fviz_nbclust(na.omit(cars[,3:16]),kmeans,method="gap_stat")
+kmean.clust <- kmeans(na.omit(cars[,3:16]),3)
+aggregate(na.omit(cars[,3:16]),by=list(kmean.clust$cluster),FUN=mean)
+new.data <- data.frame(na.omit(cars), kmean.clust$cluster)
+fviz_cluster(kmean.clust,data=na.omit(cars[,3:16]))
---
+++ stat_assignment3_R/tutorial scripts & data/correspondenceAnalysis.R
@@ -0,0 +1,52 @@
+library(factoextra)
+library(FactoMineR)
+library(graphics)
+
+example <- read_csv("C:/Users/jonathan/Desktop/DAPT Docs/spring 2019/Statistics/stat_assignments_spring19_R/stat_assignment3_R/tutorial scripts & data/CorrespondenceAnalysis.txt")
+cont.table <- xtabs(example$Count ~ example$Eye.Color+example$Hair.Color)
+
+mosaicplot(cont.table,shade=TRUE, las=2)
+chisq <- chisq.test(cont.table)
+chisq
+
+ca <- CA(cont.table,graph=FALSE) #correspondence analysis
+summary(ca)
+
+eigenvalues <- get_eigenvalue(ca)
+fviz_screeplot(ca)
+fviz_ca_biplot(ca) #Biplot
+
+fviz_contrib(ca,choice="row",axes=1)
+fviz_contrib(ca,choice="col",axes=1)
+
+#################################################################
+
+crime <- read_csv("C:/Users/jonathan/Desktop/DAPT Docs/spring 2019/Statistics/stat_assignments_spring19_R/stat_assignment3_R/tutorial scripts & data/CityCrimeData.txt")
+crime.table <- xtabs(crime$Rate ~ crime$City + crime$Crime)
+
+mosaicplot(crime.table,shade=TRUE,las=2)
+chisq <- chisq.test(crime.table)
+chisq
+
+ca <- CA(crime.table,graph=FALSE) #correspondence analysis
+summary(ca)
+
+eigenvalues <- get_eigenvalue(ca)
+fviz_screeplot(ca)
+fviz_ca_biplot(ca) #Biplot
+
+fviz_contrib(ca,choice="row",axes=1)
+fviz_contrib(ca,choice="col",axes=1)
+
+##################################################################
+#Multiple Correspondence Analysis
+
+diy <- read_csv("C:/Users/jonathan/Desktop/DAPT Docs/spring 2019/Statistics/stat_assignments_spring19_R/stat_assignment3_R/tutorial scripts & data/DoItYourself.txt")
+diy <- diy[rep(1:nrow(diy),diy$Count),]
+
+diy.mca <- MCA(diy[,1:5],graph=FALSE)
+summary(diy.mca)
+
+fviz_screeplot(diy.mca,addlabels=TRUE)
+fviz_mca_biplot(diy.mca)
+fviz_mca_var(diy.mca)
